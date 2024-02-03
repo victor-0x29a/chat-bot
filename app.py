5b41b10d83f40e3b223939853f316bf7981a8eb7
victor <cartoonbr495@gmail.com>
2024-02-03 00:10:14 -0400

    Fix: all alerts from flake8 and updates from importings

--- app.py
+++ app.py
@@ -1,25 +1,25 @@
 from config.limits import speak, getPhrase
-from config.data import listActions
-from constants import INDECISIVE_MESSAGES
+from constants import (
+    INDECISIVE_MESSAGES,
+    ERROR_MESSAGES,
+    LEARNING_LEVEL,
+    ACTIONS
+)
 
 
 class out:
     def __init__(self, stdin):
-        self.itensOrganizados = [] # uuid - total [1, 1 ,1, 1,1,1]
+        self.itensOrganizados = []
         self.choices = []
         self.std = stdin.lower().split()
         self.stdin = stdin
-        for acao in listActions():
+        for acao in ACTIONS:
             for palavras in acao["Words"]:
                 for palavra in self.std:
                     if (palavra == palavras):
                         self.choices.append(acao["UUID"])
 
-    def error(self):
-        return ["Hum, houve um erro, tente novamente mais tarde!"]
-    def MinAprendizado(self):
-        return 3
-    def coletaPorVetor(self, frase, vetor, type, initials):
+    def collect_by_vector(self, frase, vetor, type, initials):
         # frase para percorrer
         # vetor para buscar os dados depois do vetor
         # tipo de dados que ele esta buscando apos o vetor
@@ -42,21 +42,22 @@ class out:
                     self.word += letter
             return self.vector
         for word in self.stringToArray:
-                if word == vetor:
-                    self.stringToArray.remove(word)
-                    break
-                else:
-                    self.stringToArray.remove(word)
+            if word == vetor:
+                self.stringToArray.remove(word)
+                break
+            else:
+                self.stringToArray.remove(word)
         self.newArray = []
         for item in self.stringToArray:
             self.obj = item.replace(",", "")
             if type == "int":
-                self.inteiros = [1,2,3,4,5,6,7,8,9]
+                self.inteiros = [1, 2, 3, 4, 5, 6, 7, 8, 9]
                 for i in self.inteiros:
                     if self.obj == str(i):
                         self.newArray.append(self.obj)
 
         return self.newArray
+
     def start(self):
         for item in self.choices:
             self.found = False
@@ -67,17 +68,18 @@ class out:
             if not self.found:
                 self.itensOrganizados.append({"UUID": item, "total": [1]})
 
-        self.top = {"UUID": 0, "total": 0} # Here this vector of action to execute
+        self.top = {"UUID": 0, "total": 0}
+
         for item in self.itensOrganizados:
             self.total = 0
             for i in item["total"]:
-                self.total+=1
+                self.total += 1
             if self.total > self.top["total"]:
-                self.top = {"UUID": item["UUID"],"total": self.total}
+                self.top = {"UUID": item["UUID"], "total": self.total}
 
-        if (self.top["total"] < self.MinAprendizado()):
+        if (self.top["total"] < LEARNING_LEVEL):
             return speak(getPhrase(INDECISIVE_MESSAGES))
-        for item in listActions():
+        for item in ACTIONS:
             if item["UUID"] == self.top["UUID"]:
                 if not item["Actions"]["vetores"]:
                     speak(getPhrase(item["response"]))
@@ -86,12 +88,17 @@ class out:
                     return
                 for palavra in item["Actions"]["vetores"]:
                     if palavra in self.stdin:
-                        self.dados = self.coletaPorVetor(self.stdin, palavra, item["type"], item["Actions"]["vetores"])
+                        self.dados = self.collect_by_vector(
+                            self.stdin,
+                            palavra,
+                            item["type"],
+                            item["Actions"]["vetores"]
+                        )
                         if self.dados and item["action"]:
                             speak(getPhrase(item["response"]))
                             item["action"](self.dados)
                         else:
-                            speak(self.error())
+                            speak(ERROR_MESSAGES)
 
 
 while True:
