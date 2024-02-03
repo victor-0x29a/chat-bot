d6541a590bf66daa1e98e305ef7177e18b7cbbba
victor <cartoonbr495@gmail.com>
2024-02-03 00:09:28 -0400

    Feat: actions moved to constants

--- constants.py
+++ constants.py
@@ -1,3 +1,5 @@
+from config.actions import enable_airmon
+
 INDECISIVE_MESSAGES = [
     "Ops! Você pode repetir de forma clara?",
     "Talvez eu precise ter mais informações.",
@@ -9,4 +11,22 @@ INDECISIVE_MESSAGES = [
     "Poderia ser mais claro, por favor?",
     "Desculpe, não estou entendendo.",
     "Por favor, explique a questão novamente."
+]
+ERROR_MESSAGES = ["Hum, houve um erro, tente novamente mais tarde!"]
+LEARNING_LEVEL = 3
+
+ACTIONS = [
+    {
+        "UUID": 0,
+        "Words": [
+            "ative", "o",
+            "modo", "airmon",
+            "airmon-ng",
+            "rede", "inicie"
+        ],
+        "Actions": {"vetores": [""]},
+        "type": "str",
+        "response": [],
+        "action": enable_airmon
+    }
 ]
